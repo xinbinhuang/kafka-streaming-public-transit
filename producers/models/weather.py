2e2e763bd58d8e30090ef01985351ab52229e1a4
Xinbin Huang <bin.huangxb@gmail.com>
2020-04-18 01:08:16 -0700

    Add weather data via REST Proxy

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -2,13 +2,13 @@
 from enum import IntEnum
 import json
 import logging
-from pathlib import Path
 import random
-import urllib.parse
 
 import requests
 
 from models.producer import Producer
+from models.utils import load_schema
+from config import Connections, Topics
 
 
 logger = logging.getLogger(__name__)
@@ -21,25 +21,21 @@ class Weather(Producer):
         "status", "sunny partly_cloudy cloudy windy precipitation", start=0
     )
 
-    rest_proxy_url = "http://localhost:8082"
+    rest_proxy_url = Connections.REST_PROXY
 
-    key_schema = None
-    value_schema = None
+    key_schema = load_schema("weather_key.json").to_json()
+    value_schema = load_schema("weather_value.json").to_json()
 
     winter_months = set((0, 1, 2, 3, 10, 11))
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            topic_name=Topics.WEATHER,
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
+            num_partitions=2,
+            num_replicas=1,
         )
 
         self.status = Weather.status.sunny
@@ -48,17 +44,6 @@ class Weather(Producer):
             self.temp = 40.0
         elif month in Weather.summer_months:
             self.temp = 85.0
-
-        if Weather.key_schema is None:
-            with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
-                Weather.key_schema = json.load(f)
-
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
-        if Weather.value_schema is None:
-            with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
-                Weather.value_schema = json.load(f)
 
     def _set_weather(self, month):
         """Returns the current weather"""
@@ -73,37 +58,29 @@ class Weather(Producer):
     def run(self, month):
         self._set_weather(month)
 
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        resp = requests.post(
+            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+            headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
+            data=json.dumps(
+                {
+                    "key_schema": json.dumps(Weather.key_schema),
+                    "value_schema": json.dumps(Weather.value_schema),
+                    "records": [
+                        {
+                            "key": {"timestamp": self.time_millis()},
+                            "value": {
+                                "temperature": self.temp,
+                                "status": self.status.name,
+                            },
+                        }
+                    ],
+                }
+            ),
+        )
+        try:
+            resp.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            logger.error(f"Message delivered via REST Proxy failed: {err}")
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
