65f02b2606c07e29ec024b24c6ed50bc7f83c45c
Xinbin Huang <bin.huangxb@gmail.com>
2020-04-17 23:14:35 -0700

    Add station data model

--- producers/models/station.py
+++ producers/models/station.py
@@ -1,11 +1,10 @@
 """Methods pertaining to loading and configuring CTA "L" station data."""
 import logging
-from pathlib import Path
 
-from confluent_kafka import avro
-
+from config import join_topic_name, TopicPrefixes
 from models import Turnstile
 from models.producer import Producer
+from models.utils import load_schema, normalize_station_name, RecordSchema
 
 
 logger = logging.getLogger(__name__)
@@ -14,39 +13,12 @@ logger = logging.getLogger(__name__)
 class Station(Producer):
     """Defines a single station"""
 
-    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
+    key_schema: RecordSchema = load_schema("arrival_key.json")
+    value_schema: RecordSchema = load_schema("arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
-        self.name = name
-        station_name = (
-            self.name.lower()
-            .replace("/", "_and_")
-            .replace(" ", "_")
-            .replace("-", "_")
-            .replace("'", "")
-        )
-
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
-        super().__init__(
-            topic_name,
-            key_schema=Station.key_schema,
-            # TODO: value_schema=Station.value_schema, # TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
-        )
-
         self.station_id = int(station_id)
+        self.name = name
         self.color = color
         self.dir_a = direction_a
         self.dir_b = direction_b
@@ -54,26 +26,34 @@ class Station(Producer):
         self.b_train = None
         self.turnstile = Turnstile(self)
 
+        station_name = normalize_station_name(self.name)
+        topic_name = join_topic_name(TopicPrefixes.ARRIVAL, station_name)
+        super().__init__(
+            topic_name=topic_name,
+            key_schema=Station.key_schema,
+            value_schema=Station.value_schema,
+            num_partitions=3,
+            num_replicas=1,
+        )
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
-        # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
-        logger.info("arrival kafka integration incomplete - skipping")
-        #self.producer.produce(
-        #    topic=self.topic_name,
-        #    key={"timestamp": self.time_millis()},
-        #    value={
-        #        #
-        #        #
-        #        # TODO: Configure this
-        #        #
-        #        #
-        #    },
-        #)
+        try:
+            self.producer.produce(
+                topic=self.topic_name,
+                key={"timestamp": self.time_millis()},
+                value={
+                    "station_id": self.station_id,
+                    "train_id": train.train_id,
+                    "direction": direction,
+                    "line": self.color.name,
+                    "train_status": train.status.name,
+                    "prev_station_id": prev_station_id,
+                    "prev_direction": prev_direction,
+                },
+            )
+        except Exception as exc:
+            logger.error(f"Failed to send message to Kafka: `{exc}`")
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
@@ -101,4 +81,4 @@ class Station(Producer):
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
         self.turnstile.close()
-        super(Station, self).close()
+        super().close()
