ca4ddb8c5b7fb83b7e7555b678b153385bfdac82
Xinbin Huang <bin.huangxb@gmail.com>
2020-04-19 00:35:36 -0700

    Add faust stream app

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -3,8 +3,12 @@ import logging
 
 import faust
 
+import config
+
 
 logger = logging.getLogger(__name__)
+
+KAFKA_BROKER = config.Connections.KAFKA_BROKER.replace("PLAINTEXT", "kafka")
 
 
 # Faust will ingest records from Kafka in this format
@@ -29,29 +33,44 @@ class TransformedStation(faust.Record):
     line: str
 
 
-# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
-#   places it into a new topic with only the necessary information.
-app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
-# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
-# TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
-# TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+app = faust.App("stations-stream", broker=KAFKA_BROKER, store="memory://")
+
+topic = app.topic(config.Topics.STATIONS, value_type=Station)
+out_topic = app.topic(
+    config.Topics.STATIONS_LINE,
+    key_type=int,
+    value_type=TransformedStation,
+    partitions=1,
+)
+
+table = app.Table(
+    name=config.Topics.STATIONS_LINE,
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=out_topic,
+)
 
 
-#
-#
-# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
-# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
-# then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def transform_station(stations):
+    async for station in stations:
+        if station.red:
+            line = "red"
+        elif station.green:
+            line = "green"
+        elif station.blue:
+            line = "blue"
+        else:
+            line = None
+
+        transformed_station = TransformedStation(
+            station_id=station.station_id,
+            station_name=station.station_name,
+            order=station.order,
+            line=line,
+        )
+
+        table[station.station_id] = transformed_station
 
 
 if __name__ == "__main__":
